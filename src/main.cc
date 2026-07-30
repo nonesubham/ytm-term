613b2d3659d76244c5ef3a7519ce9a9af197c39d
Subham Saurav <myselfsubhamsaurav@gmail.com>
2026-07-30 11:40:44 +0530

    reworking again

---
+++ src/main.cc
@@ -0,0 +1,21 @@
+// #include <mpv/client.h>
+// #include <ftxui/dom/elements.hpp>
+// #include <ftxui/screen/screen.hpp>
+// #include <cstdio>
+
+// int main() {
+//   // mpv
+//   mpv_handle *mpv = mpv_create();
+//   if (!mpv) { std::fprintf(stderr, "mpv_create failed\n"); return 1; }
+//   mpv_terminate_destroy(mpv);
+
+//   // ftxui
+//   using namespace ftxui;
+//   auto doc = paragraph("hello from ftxui + mpv");
+//   auto screen = Screen::Create(Dimension::Fixed(40), Dimension::Fixed(3));
+//   Render(screen, doc);
+//   screen.Print();
+
+//   return 0;
+// }
+//
